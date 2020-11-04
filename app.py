c2da8e2ee6052baae96fa895acf247999d7def10
Lawrence Dovin <lawrencedovin@gmail.com>
2020-11-04 15:19:20 -0500

    game board is stored into session

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, render_template, redirect, flash, jsonify, session
+from flask import Flask, request, render_template, redirect, flash, jsonify
+from flask import session, make_response
 from flask_debugtoolbar import DebugToolbarExtension
 from boggle import Boggle
 
@@ -12,4 +13,19 @@ boggle_game = Boggle()
 @app.route('/')
 def show_board():
     """Show boggle board"""
-    return render_template('game.html', boggle_game=boggle_game)
+    # boggle_game_board = session['game_board']
+    # boggle_game_board.append(boggle_game.make_board())
+    # session['game_board'] = boggle_game_board
+    session['game_board'] = boggle_game.make_board()
+
+    return render_template('game.html')
+
+@app.route('/test')
+def show_test():
+    """Show boggle board"""
+    return render_template('test.html')
+
+@app.route('/test2')
+def show_test2():
+    """Show boggle board"""
+    return render_template('test2.html')
