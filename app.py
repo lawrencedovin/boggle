17db45ab3871d33aaa96cb3bdeaed34cf55b930d
Lawrence Dovin <lawrencedovin@gmail.com>
2020-11-04 11:27:40 -0500

    Added flask libraries to app

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+from flask import Flask, request, render_template, redirect, flash, jsonify, session
+from flask_debugtoolbar import DebugToolbarExtension
 from boggle import Boggle
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "abc123"
+app.debug = False
+toolbar = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
