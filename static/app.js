7029ea4fe4fcd08056320144bc66ac26ea07e2c0
Lawrence Dovin <lawrencedovin@gmail.com>
2020-11-05 17:50:22 -0500

    Appended result to view

--- static/app.js
+++ static/app.js
@@ -2,21 +2,28 @@ const $form = $('form');
 let $guessButton = $('#guess-button');
 let $guessInput = $('#guess-input');
 
-async function getAxiosRequest() {
+async function checkGuess() {
     let guess = $guessInput.val();
     try {
         const response = await axios.get('/check-guess', { params: { guess: guess }});
         const {data} = response;
-        console.log(data.result);
+        appendResultToView(data.result);
     } catch (e) {
         alert(`Error: ${e}`);
     }
 }
 
+function appendResultToView(result) {
+    const $result = $('#result');
+    if(result === 'ok') $result.text('Correct!');
+    else if(result === 'not-on-board') $result.text('Not on board');
+    else $result.text('Not a word')
+}
+
 $form.on('submit', (e) => {
     e.preventDefault();
     if($guessInput.val()) {
-        getAxiosRequest();
+        checkGuess();
         $guessInput.val('');
     }
     else alert("🤠 Yeehaw, we decent folks don't like your no value round here.");
