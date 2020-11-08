263139f1c4d91ebb20ab1ab90fc76da8c2b92499
Lawrence Dovin <lawrencedovin@gmail.com>
2020-11-08 17:39:23 -0500

    Semi OOP countdown not working

--- static/app.js
+++ static/app.js
@@ -6,90 +6,110 @@ const $currentScore = $('#current-score');
 const $highScore = $('#high-score');
 const $result = $('#result');
 const $correctWords = $('#correct-words');
-let currentScore = 0;
-let seconds = 30;
-let words = [];
-const colors = ["#00ADD2", "#00FF0A"];
+// let currentScore = 0;
+// let seconds = 30;
+// let words = [];
+// const colors = ["#00ADD2", "#00FF0A"];
 
-async function checkGuess() {
-    let guess = $guessInput.val().toLowerCase();
-    try {
-        const response = await axios.get('/check-guess', { params: { guess: guess }});
-        const {result} = response.data; 
-        appendResultsToView(result, guess);
-    } catch (e) {
-        alert(`Error: ${e}`);
+class Boggle {
+    constructor(seconds = 30) {
+        this.seconds = seconds;
+        this.currentScore = 0;
+        this.words = [];
+        this.colors = ["#00ADD2", "#00FF0A"];
+        // this.countdown();
+        $(document).ready(this.countdown);
+        $(".guess").on("submit", this.FormSubmit.bind(this));
     }
-}
 
-function appendResultsToView(result, guess) {
-    $('.result').css('display', 'inline-block');
-    if(result === 'ok'){ 
-        if(jQuery.inArray(guess.toUpperCase(), words) != -1){
-            $result.text('You already guessed that word.');
-            $('.result').addClass('incorrect');
+    async FormSubmit(e){
+        e.preventDefault();
+        if($guessInput.val().toLowerCase()) {
+            this.checkGuess();
+            $guessInput.val('');
+        }
+        else alert("🤠 Yeehaw, we decent folks don't like your no value round here.");
+    }
+
+    async checkGuess() {
+        let guess = $guessInput.val().toLowerCase();
+        try {
+            const response = await axios.get('/check-guess', { params: { guess: guess }});
+            const {result} = response.data; 
+            this.appendResultsToView(result, guess);
+        } catch (e) {
+            alert(`Error: ${e}`);
+        }
+    }
+
+    appendResultsToView(result, guess) {
+        $('.result').css('display', 'inline-block');
+        if(result === 'ok'){ 
+            if(jQuery.inArray(guess.toUpperCase(), this.words) != -1){
+                $result.text('You already guessed that word.');
+                $('.result').addClass('incorrect');
+            }
+            else {
+                $result.text('Correct!'); 
+                this.currentScore += (guess.length * 10);
+                $currentScore.text(`Score: ${this.currentScore}`);
+                this.words.push(guess.toUpperCase());
+                $correctWords.text(`Correct words: ${this.words}`);
+                $('.result').removeClass('incorrect'); 
+            }      
         }
         else {
-            $result.text('Correct!'); 
-            currentScore += (guess.length * 10);
-            $currentScore.text(`Score: ${currentScore}`);
-            words.push(guess.toUpperCase());
-            $correctWords.text(`Correct words: ${words}`);
-            $('.result').removeClass('incorrect'); 
-        }      
+            if(result === 'not-on-board') $result.text('Not on board');
+            else $result.text('Not a word');
+            $('.result').addClass('incorrect');
+        }
     }
-    else {
-        if(result === 'not-on-board') $result.text('Not on board');
-        else $result.text('Not a word');
-        $('.result').addClass('incorrect');
-    }
-}
 
-$form.on('submit', (e) => {
-    e.preventDefault();
-    if($guessInput.val().toLowerCase()) {
-        checkGuess();
-        $guessInput.val('');
-    }
-    else alert("🤠 Yeehaw, we decent folks don't like your no value round here.");
-})
-
-async function scoreGame() {
-    const resp = await axios.post('/post-score', { score: currentScore });
-    if (resp.data.brokeRecord){
-        $('.result').removeClass('incorrect');
-        $('.result').addClass('broke-record');
-        changeColor(colors, 0);
-        $result.text(`👾 New High Score: ${currentScore} 👾`);
-        $highScore.text(`High Score: ${currentScore}`);
-    } 
-  }
-
-function countdown() {
-    let countdown = setInterval(async function() {
-        if(seconds === 0) {
-            // Clears input and disables input value and submit button
-            $guessInput.val('');
-            $guessInput.prop('disabled', true);
-            $guessButton.prop('disabled', true);
-            clearInterval(countdown);
-            await scoreGame();
+    async scoreGame() {
+        const resp = await axios.post('/post-score', { score: this.currentScore });
+        if (resp.data.brokeRecord){
+            $('.result').removeClass('incorrect');
+            $('.result').addClass('broke-record');
+            this.changeColor(this.colors, 0);
+            $result.text(`👾 New High Score: ${this.currentScore} 👾`);
+            $highScore.text(`High Score: ${this.currentScore}`);
         } 
-        $countdown.text(`${seconds}`)
-        seconds--;
-    }, 1000);
+    }
+
+    async countdown() {
+        let countdown = setInterval(async function() {
+            if(this.seconds === 0) {
+                // Clears input and disables input value and submit button
+                $guessInput.val('');
+                $guessInput.prop('disabled', true);
+                $guessButton.prop('disabled', true);
+                clearInterval(countdown);
+                await this.scoreGame();
+            } 
+            $countdown.text(`${this.seconds}`)
+            this.seconds--;
+        }, 1000);
+    }
+
+    changeColor(colors, i) {
+        setInterval(() => {
+          $($result).css('color', colors[i]);
+          i++;
+          i %= colors.length;
+        }, 250);
+    }
+    
 }
 
-function changeColor(colors, i) {
-    setInterval(() => {
-      $($result).css('color', colors[i]);
-      i++;
-      i %= colors.length;
-    }, 250);
-}
+new Boggle();
 
 
-$( document ).ready(countdown);
+
+
+
+
+
+
 
 
 
